5f0173b2b44a90806ba1cbf057db168043660036
arminmoh93 <arminmoh93@gmail.com>
2022-11-02 14:38:49 +0330

    firit commit

---
+++ portfolio-front/src/myHeader.js
@@ -0,0 +1,8 @@
+function MyHeader() {
+    return ( < div className = "blue-header" >
+        <
+        div > I 'm Armin </div> <div className = "detailed-info" > , this is my portfolio hope you like it! </div>  </div > )
+    };
+
+
+    export default MyHeader;
